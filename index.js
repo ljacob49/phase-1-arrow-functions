33543d11d209cde6d3cd9bd6323a17f09a3f4c67
Louis Jacob <loujacob2@gmail.com>
2021-10-13 08:09:30 -0400

    commit tree 5b316f532ce52b73851de3d304e6fd43a638e202

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1,parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 / parameter2;
+}
+divide (2000,100);
